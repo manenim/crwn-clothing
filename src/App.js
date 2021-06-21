5c143e2fdafae0dc0da552723c90170ee8ebc367
manex <manexudoh@gmail.com>
2021-06-21 07:19:40 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './Homepage.components';
+import Homepage from '../src/pages/homepage/Homepage.components';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path = '/' component = {Homepage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/menu-item/Menu-item.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import './menu-item.styles.scss'
+
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
+    return (
+        <div className = {`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
+            <div style = {{
+            backgroundImage: `url(${imageUrl})`
+        }}  className = "background-image">
+
+            </div>
+            <div className = "content">
+                <h1 className = "tite">{title.toUpperCase()}</h1>
+                <span className = "subtitle">Shop Now</span>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(MenuItem)
